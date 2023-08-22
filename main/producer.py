1d03c0b59e9b32a6cd10768dbf5d4e5ea961c30f
turkin501 <19522155@gm.uit.edu.vn>
2023-08-23 05:36:14 +0700

    init

---
+++ main/producer.py
@@ -0,0 +1,12 @@
+import pika, json
+
+params = pika.URLParameters('amqps://fhwpxkma:QT7w_DVKY1BTNx8VgmqooTC-e_ChH5A7@woodpecker.rmq.cloudamqp.com/fhwpxkma')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
